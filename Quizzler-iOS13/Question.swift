655fe0d7ec71706e316e7a253674021eb3d77182
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-14 17:10:57 -0600

    99. Creating a Question Struct

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Isaac Urbina on 11/14/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+struct Question {
+	let question: String
+	let answer: String
+	
+	init(q: String, a: String) {
+		self.question = q
+		self.answer = a
+	}
+}
+
